f35c446da3f3e7538b4b321a132a1bd9499af42a
serbru20066 <bruno@Mac-Pro-de-bruno.local>
2014-05-14 15:48:44 -0500

    Initial Commit

---
+++ proyectoFarmaciOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  proyectoFarmaciOS
+//
+//  Created by bruno on 14/05/14.
+//  Copyright (c) 2014 Bruno Cardenas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
